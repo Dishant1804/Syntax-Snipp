7fcdcc554b5d48c14c125e16051462385440c838
Dishant1804 <dishantmiyani1804@gmail.com>
2024-09-06 14:18:23 +0530

    routes

--- backend/api/v1/route.js
+++ backend/api/v1/route.js
@@ -1,27 +1,71 @@
 const express = require('express');
+const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const JWT = require('jsonwebtoken');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
+router.use(helmet());
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const prisma = new PrismaClient();
 
-const app = express();
+const signInLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: "Too many login attempts. Please try again later.",
+});
 
-app.use(express.json());
+router.post('/signup', async (req, res) => {
+  const { email, username, password } = req.body;
+  const saltRounds = 12;
 
-app.get('/helloworld', (_req, res) => {
-  res.send('hello world');
-}
-);
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
-app.post('/signup', async (req, res) => {
-  const { name, username, password } = req.body;
+  try {
+    await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        passwordHash: passwordHash
+      }
+    });
 
-  const user = await prisma.user.create({
-    data: {
-      name: name,
-      username: username,
-      password: password
+    return res.json({"Message" : "Sign-up Successful"});
+  }
+  catch (e) {
+    return res.status(400).json({ "error": e.message });
+  }
+});
+
+router.post('/signin', signInLimiter , async(req , res) =>{
+  const {email , password} = req.body;
+  
+  try{
+    const user = await prisma.user.findUnique({
+      where :{
+        email : email
+      },
+      select : {
+        id : true,
+        passwordHash : true
+      }
+    });
+
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+
+    if (!user || !isPasswordValid) {
+      return res.status(401).json({ "message": "Invalid Credentials" });
     }
-  });
 
-  res.json(user);
-} );  
+    const token = JWT.sign({userId : user.id} , JWT_SECRET);
+    return res.json({"message" : "Sign-in Successful" , "token" : token}); 
+  }
+  catch(e){
+    return res.status(401).json({ "message" :"Invalid Credentials"});
+  }
+})
+
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const apiV1 = require('./api/v1/route');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api/v1', apiV1);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
