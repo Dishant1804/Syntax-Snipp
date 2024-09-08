69af9f7bf4fc26e2a70de7fd60e38b1806c8bae6
Dishant1804 <dishantmiyani1804@gmail.com>
2024-09-08 21:52:00 +0530

    ui lib installation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,12 @@
-"use client"
-
-import * as React from "react";
-import { useUser } from "@clerk/nextjs";
 
 export default function Home() {
-  const { isLoaded, isSignedIn, user } = useUser();
-
-  if (!isLoaded || !isSignedIn) {
-    return <div>Loading or not signed in...</div>;
+  const handleClick = () =>{
+    window.alert("Hello fckers");
   }
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Clerk User Details</h1>
-      <ul className="space-y-2">
-        <li><strong>ID:</strong> {user.id}</li>
-        <li><strong>First Name:</strong> {user.firstName}</li>
-        <li><strong>Last Name:</strong> {user.lastName}</li>
-        <li><strong>Email:</strong> {user.primaryEmailAddress?.emailAddress}</li>
-        <li><strong>Username:</strong> {user.username}</li>
-      </ul>
+  <div>
+      Hello world
     </div>
   );
 }
