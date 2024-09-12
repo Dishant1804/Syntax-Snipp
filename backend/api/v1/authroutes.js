11721af20f05c1238e047506f9351398e67feb48
Dishant1804 <dishantmiyani1804@gmail.com>
2024-09-12 19:27:12 +0530

    DB and schema changes

--- backend/api/v1/authroutes.js
+++ backend/api/v1/authroutes.js
@@ -4,7 +4,7 @@ import { hash, compare } from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
-import authMiddleware from './middleware/authMiddleware.js';
+import authMiddleware from './middleware/authMiddleware.js'; //update the middleware after changing the schema
 import { z } from 'zod';
 import passport from './config/passportConfig.js';
 
@@ -19,14 +19,14 @@ const prisma = new PrismaClient();
 const saltRounds = 12;
 
 const userSignupSchema = z.object({
-  username : z.string().toLowerCase().max(50).min(3),
-  email : z.string().email(),
-  password : z.string().min(8).max(50) 
+  username: z.string().toLowerCase().max(50).min(3),
+  email: z.string().email(),
+  password: z.string().min(8).max(50)
 });
 
 const userSigninSchema = z.object({
-  email : z.string().email(),
-  password : z.string().min(8).max(50)
+  email: z.string().email(),
+  password: z.string().min(8).max(50)
 })
 
 const signInLimiter = rateLimit({
@@ -39,13 +39,16 @@ const createToken = (userId) => {
   return jwt.sign({ userId }, JWT_SECRET, { expiresIn: '30d' });
 }
 
+/**
+ * update after schema change
+ */
 router.post('/signup', async (req, res) => {
   const body = req.body;
 
   const validation = userSignupSchema.safeParse(body);
 
-  if(!validation.success){
-    return res.status(401).json({"message" : "Enter proper credentials", "success" : false});
+  if (!validation.success) {
+    return res.status(401).json({ "message": "Enter proper credentials", "success": false });
   }
 
   const passwordHash = await hash(req.body.password, saltRounds);
@@ -59,11 +62,11 @@ router.post('/signup', async (req, res) => {
       }
     });
 
-    return res.status(201).json({ "status" : "signedup" , "success" : true});
-  } 
+    return res.status(201).json({ "status": "signedup", "success": true });
+  }
   catch (e) {
     console.log(e);
-    return res.status(400).json({ "error": e.message , "success" : false});
+    return res.status(400).json({ "error": e.message, "success": false });
   }
 });
 
@@ -71,8 +74,8 @@ router.post('/signin', signInLimiter, async (req, res) => {
   const body = req.body;
   const validation = userSigninSchema.safeParse(body);
 
-  if(!validation.success){
-    return res.status(401).json({"message" : "Enter proper credentials" , "success" : false});
+  if (!validation.success) {
+    return res.status(401).json({ "message": "Enter proper credentials", "success": false });
   }
 
   try {
@@ -87,37 +90,50 @@ router.post('/signin', signInLimiter, async (req, res) => {
     });
 
     if (!user) {
-      return res.status(401).json({ "message": "Invalid Credentials" , "success" : false});
+      return res.status(401).json({ "message": "Invalid Credentials", "success": false });
     }
 
     const isPasswordValid = await compare(req.body.password, user.passwordHash);
 
     if (!isPasswordValid) {
-      return res.status(401).json({ "message": "Invalid Credentials" ,"success" : false});
+      return res.status(401).json({ "message": "Invalid Credentials", "success": false });
     }
 
     const token = createToken(user.id)
-    return res.json({ "status": "signedin", "token": token , "success" : true});
-  } 
+    return res.json({ "status": "signedin", "token": token, "success": true });
+  }
   catch (e) {
     console.log(e);
-    return res.status(500).json({ "message": "Internal Server Error" , "success" : false});
+    return res.status(500).json({ "message": "Internal Server Error", "success": false });
   }
 });
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
 
-router.get('/google/callback', passport.authenticate('google', { session: false, failureRedirect: '/login' }),
+/**
+ * add proper redirect to the required page --> figure out what needs to be changed
+ */
+router.get('/google/callback', passport.authenticate('google', {
+  session: false,
+  failureRedirect: '/login'
+}),
   (req, res) => {
-    res.json({ "status": "Google authentication successful", token: req.user.token , "success" : true });
+    res.json({ "status": "Google authentication successful", token: req.user.token, "success": true });
   }
 );
 
-router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
+router.get('/github', passport.authenticate('github', {
+  scope: ['read:user', 'gist']
+}));
 
-router.get('/github/callback', passport.authenticate('github', { session: false, failureRedirect: '/login' }),
+router.get('/github/callback', passport.authenticate('github', {
+  session: false,
+  failureRedirect: '/login'
+}),
   (req, res) => {
-    res.json({ "status" : "GitHub authentication successful", token : req.user.token , "success" : true });
+    res.json({ "status": "GitHub authentication successful", token: req.user.token, "success": true });
   }
 );
 
@@ -133,7 +149,7 @@ router.patch('/updateprofile', authMiddleware, signInLimiter, async (req, res) =
     });
 
     if (!userToUpdate) {
-      return res.status(404).json({ "error": "User not found" , "success" : false});
+      return res.status(404).json({ "error": "User not found", "success": false });
     }
 
     const updateData = {};
@@ -143,20 +159,20 @@ router.patch('/updateprofile', authMiddleware, signInLimiter, async (req, res) =
     }
 
     if (email) {
-      const existingUser = await prisma.user.findUnique({ 
+      const existingUser = await prisma.user.findUnique({
         where: {
-            email 
-          }
-        });
+          email
+        }
+      });
 
       if (existingUser && existingUser.id !== userId) {
-        return res.status(400).json({ "error": "Email already in use" , "success" : false});
+        return res.status(400).json({ "error": "Email already in use", "success": false });
       }
       updateData.email = email;
     }
 
     if (password) {
-      const saltRounds = 12; 
+      const saltRounds = 12;
       updateData.passwordHash = await hash(password, saltRounds);
     }
 
@@ -166,15 +182,15 @@ router.patch('/updateprofile', authMiddleware, signInLimiter, async (req, res) =
         data: updateData,
       });
 
-      return res.status(200).json({ "status": "updated" , "success" : true });
-    } 
-    else {
-      return res.status(400).json({ "error": "No changes to update" , "success" : false });
+      return res.status(200).json({ "status": "updated", "success": true });
     }
-  } 
+    else {
+      return res.status(400).json({ "error": "No changes to update", "success": false });
+    }
+  }
   catch (e) {
     console.error(e);
-    return res.status(500).json({ "error": "Internal server error" , "success" : false});
+    return res.status(500).json({ "error": "Internal server error", "success": false });
   }
 });
 
@@ -189,23 +205,21 @@ router.get('/user/profile', authMiddleware, async (req, res) => {
       select: {
         username: true,
         email: true,
+        isGoogle : true,
+        isGithub : true,
       }
     });
 
     if (!user) {
-      return res.status(404).json({ "error": "User not found" , "success" : false });
+      return res.status(404).json({ "error": "User not found", "success": false });
     }
 
-    return res.status(200).json({ 
-      "status": "retrieved",
-      "success" : true,
-      profile: user 
-    });
+    return res.status(200).json({ "status": "retrieved", "success": true, profile: user });
 
   }
   catch (e) {
     console.error(e);
-    return res.status(500).json({ "error": "Internal server error" , "success" : false});
+    return res.status(500).json({ "error": "Internal server error", "success": false });
   }
 });
 
--- backend/api/v1/config/passportConfig.js
+++ backend/api/v1/config/passportConfig.js
@@ -2,9 +2,18 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { Strategy as GitHubStrategy } from 'passport-github2';
 import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
 
+const generateJWT = (user) => {
+  return jwt.sign({ userId : user.id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+/**
+ * change the parameters after schema update
+ * check the profile id is it same as user ID?
+ */
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -13,7 +22,7 @@ passport.use(new GoogleStrategy({
   try {
     let user = await prisma.user.findFirst({ 
       where: { 
-        googleId: profile.id 
+        id : profile.id
       }
     });
     if (!user) {
@@ -25,13 +34,18 @@ passport.use(new GoogleStrategy({
         }
       });
     }
-    done(null, { user, token: accessToken });
+    const token = generateJWT(user);
+    done(null, { user, token });
   } 
   catch (error) {
     done(error, null);
   }
 }));
 
+/**
+ * change the parameters according to the schema this will act as link to the main account
+ * send 2 db calls one patch req to user for isGithub : true and 2nd req to github model to update the contents
+ */
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
@@ -47,14 +61,14 @@ passport.use(new GitHubStrategy({
     if (!user) {
       user = await prisma.user.create({
         data: {
-          username: profile.displayName,
+          username: profile.username,
           email: profile.emails[0].value,
           githubId: profile.id
         }
       });
     }
-
-    done(null, { user });
+    const token = generateJWT(user);
+    done(null, { user , token });
   } catch (error) {
     done(error, null);
   }
