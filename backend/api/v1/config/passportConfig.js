de51d0053e3f9e7476c5d09fa75a0a37cb05dff0
Dishant1804 <dishantmiyani1804@gmail.com>
2024-09-13 14:15:10 +0530

    passport config

--- backend/api/v1/config/passportConfig.js
+++ backend/api/v1/config/passportConfig.js
@@ -49,11 +49,16 @@ passport.use(new GoogleStrategy({
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/api/v1/auth/github/callback"
-}, async (accessToken, refreshToken, profile, done) => {
-  console.log(profile);
+  callbackURL: "http://localhost:3000/api/v1/auth/github/callback",
+  passReqToCallback : true,
+}, async (req , accessToken, refreshToken, profile, done) => {
+  const { state } = req.query
   try {
-    done(null, { profile , token : accessToken });
+    const decodedState = JSON.parse(decodeURIComponent(state));
+    const { userId } = decodedState;
+
+    const data = { profile, token: accessToken, userId };
+    done(null, data);
   } catch (error) {
     done(error, null);
   }
