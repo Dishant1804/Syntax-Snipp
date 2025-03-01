25b66472a587942f2e2a56b7fb947a4bc2011371
Dishant1804 <dishantmiyani1804@gmail.com>
2025-03-01 13:33:16 +0530

    sidebar fix

--- frontend/app/createsnippet/page.tsx
+++ frontend/app/createsnippet/page.tsx
@@ -10,7 +10,7 @@ const CreateSnippet = () => {
     <div className="h-screen w-screen bg-[#111111] flex flex-row justify-center">
       <ResizablePanelGroup direction="horizontal" className="">
         <ResizablePanel minSize={18} defaultSize={18}  maxSize={24}>
-          <Sidebar />
+          <Sidebar isCollapsed={false} />
         </ResizablePanel>
         <ResizableHandle className="h-screen bg-slate-400/20 " />
         <ResizablePanel minSize={24} defaultSize={82} >
--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -13,7 +13,7 @@ const Profile = () => {
   return <div className="h-screen w-screen bg-[#111111] flex flex-row justify-center">
     <ResizablePanelGroup direction="horizontal" className="">
       <ResizablePanel minSize={18} defaultSize={18} maxSize={24}>
-        <Sidebar />
+        <Sidebar isCollapsed={false}/>
       </ResizablePanel>
       <ResizableHandle className="h-screen bg-slate-400/20 " />
       <ResizablePanel minSize={64} defaultSize={82} >
